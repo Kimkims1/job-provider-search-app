3c2cb73a5c564f3282adc0955cc6446afe50f8ec
Kimkims1 <kimanthikevin76@gmail.com>
2020-02-27 15:49:34 +0300

    log out

--- app/src/main/java/com/carldroid/itjobs/Dashboard.java
+++ app/src/main/java/com/carldroid/itjobs/Dashboard.java
@@ -1,8 +1,13 @@
 package com.carldroid.itjobs;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.logout_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //Handle items handled
+        if (item.getItemId() == R.id.action_logout) {
+            Intent intent = new Intent(this, Login.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
