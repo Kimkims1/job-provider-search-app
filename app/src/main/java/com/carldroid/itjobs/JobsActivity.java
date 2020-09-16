c8dbdeeb3bfd3efec23253601b35a8e4ad3cfe08
Kimkims1 <kimanthikevin76@gmail.com>
2020-09-17 01:05:58 +0300

    adapter

--- app/src/main/java/com/carldroid/itjobs/JobsActivity.java
+++ app/src/main/java/com/carldroid/itjobs/JobsActivity.java
@@ -11,14 +11,17 @@ import com.carldroid.itjobs.adapters.JobAdapter;
 import com.carldroid.itjobs.models.JobModel;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.firebase.ui.firestore.paging.FirestorePagingOptions;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
 public class JobsActivity extends AppCompatActivity {
 
     private RecyclerView jobRecyclerView;
-    private FirebaseFirestore firestore;
     private JobAdapter adapter;
+
+    private FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+    private CollectionReference reference = firestore.collection("Jobs");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +32,34 @@ public class JobsActivity extends AppCompatActivity {
 
         jobRecyclerView = findViewById(R.id.rec_jobs);
 
+        loadData();
+
+    }
+
+    private void loadData() {
+        Query query = reference.orderBy("jobBudget", Query.Direction.DESCENDING);
+
+        FirestoreRecyclerOptions<JobModel> options = new FirestoreRecyclerOptions.Builder<JobModel>()
+                .setQuery(query, JobModel.class)
+                .build();
+
+        adapter = new JobAdapter(options);
+
+        RecyclerView recyclerView = findViewById(R.id.rec_jobs);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }
