9432bf99638731a319404f8b6122394020729b21
Kimkims1 <kimanthikevin76@gmail.com>
2020-09-17 12:17:09 +0300

    codes39-extra activities added

---
+++ app/src/main/java/com/carldroid/itjobs/OthersActivity.java
@@ -0,0 +1,14 @@
+package com.carldroid.itjobs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OthersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_others);
+    }
+}
--- app/src/main/java/com/carldroid/itjobs/ui/home/HomeFragment.java
+++ app/src/main/java/com/carldroid/itjobs/ui/home/HomeFragment.java
@@ -11,12 +11,15 @@ import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import com.carldroid.itjobs.AppliedActivity;
+import com.carldroid.itjobs.ConfirmedActivity;
 import com.carldroid.itjobs.JobsActivity;
+import com.carldroid.itjobs.OthersActivity;
 import com.carldroid.itjobs.R;
 
 public class HomeFragment extends Fragment {
 
-    private CardView jobs, applied, others;
+    private CardView jobs, applied, confirmed, others;
 
     public HomeFragment() {
         //Required Empty constructor
@@ -33,11 +36,38 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         jobs = view.findViewById(R.id.jobs);
+        applied = view.findViewById(R.id.applied);
+        confirmed = view.findViewById(R.id.confirmed);
+        others = view.findViewById(R.id.others);
 
         jobs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getContext(), JobsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        applied.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), AppliedActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        confirmed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), ConfirmedActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        others.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), OthersActivity.class);
                 startActivity(intent);
             }
         });
