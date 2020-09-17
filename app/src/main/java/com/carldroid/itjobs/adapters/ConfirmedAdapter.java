0fdb83097a16834705e431d0840e4abf73c82c7e
Kimkims1 <kimanthikevin76@gmail.com>
2020-09-17 21:06:25 +0300

    codes39-extra applying jobs

--- app/src/main/java/com/carldroid/itjobs/adapters/ConfirmedAdapter.java
+++ app/src/main/java/com/carldroid/itjobs/adapters/ConfirmedAdapter.java
@@ -1,33 +1,25 @@
 package com.carldroid.itjobs.adapters;
 
 import android.content.Context;
-import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.carldroid.itjobs.AppliedActivity;
 import com.carldroid.itjobs.R;
 import com.carldroid.itjobs.models.AppliedModel;
-import com.carldroid.itjobs.models.JobModel;
+import com.carldroid.itjobs.models.ConfirmedModel;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class AppliedAdapter extends FirestoreRecyclerAdapter<AppliedModel, AppliedAdapter.applyViewHolder> {
+public class ConfirmedAdapter extends FirestoreRecyclerAdapter<ConfirmedModel, ConfirmedAdapter.confirmViewHolder> {
 
     private Context context;
     private CollectionReference reference;
@@ -40,13 +32,13 @@ public class AppliedAdapter extends FirestoreRecyclerAdapter<AppliedModel, Appli
      * @param options
 
      */
-    public AppliedAdapter(@NonNull FirestoreRecyclerOptions<AppliedModel> options, Context context) {
+    public ConfirmedAdapter(@NonNull FirestoreRecyclerOptions<ConfirmedModel> options, Context context) {
         super(options);
         this.context = context;
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull final applyViewHolder holder, int position, @NonNull final AppliedModel model) {
+    protected void onBindViewHolder(@NonNull final confirmViewHolder holder, int position, @NonNull final ConfirmedModel model) {
         holder.jobBudget.setText("Budget: " + model.getJobBudget());
         holder.jobDescription.setText("Description: " + "\n" + model.getJobDescription());
         holder.jobDuration.setText("Time: " + model.getJobDuration());
@@ -58,13 +50,13 @@ public class AppliedAdapter extends FirestoreRecyclerAdapter<AppliedModel, Appli
 
     @NonNull
     @Override
-    public applyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_applied, parent, false);
+    public confirmViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_confirmed, parent, false);
 
-        return new applyViewHolder(view);
+        return new confirmViewHolder(view);
     }
 
-    public class applyViewHolder extends RecyclerView.ViewHolder {
+    public class confirmViewHolder extends RecyclerView.ViewHolder {
 
         private TextView jobTitle;
         private TextView jobDescription;
@@ -75,7 +67,7 @@ public class AppliedAdapter extends FirestoreRecyclerAdapter<AppliedModel, Appli
         private TextView idNumber;
         private RelativeLayout mainLayout;
 
-        public applyViewHolder(@NonNull View itemView) {
+        public confirmViewHolder(@NonNull View itemView) {
             super(itemView);
 
             jobBudget = itemView.findViewById(R.id.budget);
