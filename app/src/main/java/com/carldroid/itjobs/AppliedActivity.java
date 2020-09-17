ab4ca734e2350be7a9e52e00bcd4cf8049fe0c02
Kimkims1 <kimanthikevin76@gmail.com>
2020-09-17 12:41:27 +0300

    codes39-extra activities added

--- app/src/main/java/com/carldroid/itjobs/AppliedActivity.java
+++ app/src/main/java/com/carldroid/itjobs/AppliedActivity.java
@@ -1,14 +1,80 @@
 package com.carldroid.itjobs;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.carldroid.itjobs.adapters.JobAdapter;
+import com.carldroid.itjobs.models.JobModel;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
 public class AppliedActivity extends AppCompatActivity {
+
+    private RecyclerView jobRecyclerView;
+    private JobAdapter adapter;
+
+    private FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+    private CollectionReference reference = firestore.collection("AppliedJobs");
+
+    private ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_applied);
+
+        firestore = FirebaseFirestore.getInstance();
+        jobRecyclerView = findViewById(R.id.rec_jobs);
+
+        //init actionbar
+        actionBar = getSupportActionBar();
+
+        //title
+        actionBar.setTitle("Applied");
+
+        //add back button
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        loadData();
+    }
+
+    private void loadData() {
+        Query query = reference.orderBy("idNumber", Query.Direction.DESCENDING);
+
+        FirestoreRecyclerOptions<JobModel> options = new FirestoreRecyclerOptions.Builder<JobModel>()
+                .setQuery(query, JobModel.class)
+                .build();
+
+        adapter = new JobAdapter(options,this);
+
+        RecyclerView recyclerView = findViewById(R.id.rec_jobs);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed(); //takes you back to previous activity
+        return super.onSupportNavigateUp();
     }
 }
---
+++ app/src/main/java/com/carldroid/itjobs/adapters/AppliedAdapter.java
@@ -0,0 +1,144 @@
+package com.carldroid.itjobs.adapters;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.carldroid.itjobs.R;
+import com.carldroid.itjobs.models.JobModel;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class AppliedAdapter extends FirestoreRecyclerAdapter<JobModel, AppliedAdapter.jobViewHolder> {
+
+    private Context context;
+    private CollectionReference reference;
+    private FirebaseFirestore firestore;
+
+    /**
+     * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
+     * FirestoreRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public AppliedAdapter(@NonNull FirestoreRecyclerOptions<JobModel> options, Context context) {
+        super(options);
+        this.context = context;
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull final jobViewHolder holder, int position, @NonNull final JobModel model) {
+        holder.jobBudget.setText("Budget: " + model.getJobBudget());
+        holder.jobDescription.setText("Description: " + "\n" + model.getJobDescription());
+        holder.jobDuration.setText("Time: " + model.getJobDuration());
+        holder.payMethod.setText("Payment: " + model.getPayMethod());
+        holder.jobTitle.setText("Title: " + model.getJobTitle());
+        holder.idNumber.setText("Order id: " + model.getIdNumber());
+
+       /* holder.btnApply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                CollectionReference collectionReference;
+
+                firestore = FirebaseFirestore.getInstance();
+
+                if (user != null) {
+                    // Name, email address, and profile photo Url
+                    String name = user.getDisplayName();
+                    String email = user.getEmail();
+                    Uri photoUrl = user.getPhotoUrl();
+
+                    // Check if user's email is verified
+                    boolean emailVerified = user.isEmailVerified();
+
+                    // The user's ID, unique to the Firebase project. Do NOT use this value to
+                    // authenticate with your backend server, if you have one. Use
+                    // FirebaseUser.getIdToken() instead.
+                    String uid = user.getUid();
+
+                    collectionReference = firestore.collection("ApplyTest").document("Users").collection(email);
+
+                    String jobBudget = model.getJobBudget();
+                    String jobDescription = model.getJobDescription();
+                    String jobDuration = model.getJobDuration();
+                    long idNumber = model.getIdNumber();
+                    String payMethod = model.getPayMethod();
+                    String jobTitle = model.getJobTitle();
+
+                    JobModel jobModel = new JobModel(jobTitle, jobDescription, jobBudget, jobDuration, payMethod, idNumber);
+
+                    collectionReference.add(jobModel)
+                            .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                                @Override
+                                public void onComplete(@NonNull Task<DocumentReference> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(context, "Job applied! You will receive an email or phone call with further instructions", Toast.LENGTH_SHORT).show();
+
+                                       // holder.mainLayout.setVisibility(View.INVISIBLE);
+                                    } else {
+                                        Toast.makeText(context, "Application failed: " + task.getException(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+
+
+                //collectionReference = firestore.collection("AppliedJobs");
+
+
+            }
+        });*/
+    }
+
+    @NonNull
+    @Override
+    public jobViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_applied, parent, false);
+
+        return new jobViewHolder(view);
+    }
+
+    public class jobViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView jobTitle;
+        private TextView jobDescription;
+        private TextView jobDuration;
+        private TextView payMethod;
+        private Button btnApply;
+        private TextView jobBudget;
+        private TextView idNumber;
+        private RelativeLayout mainLayout;
+
+        public jobViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            jobBudget = itemView.findViewById(R.id.budget);
+            jobDescription = itemView.findViewById(R.id.jobDescription);
+            jobDuration = itemView.findViewById(R.id.jobDuration);
+            payMethod = itemView.findViewById(R.id.payMethod);
+            jobTitle = itemView.findViewById(R.id.jobTitle);
+            btnApply = itemView.findViewById(R.id.btnApply);
+            idNumber = itemView.findViewById(R.id.idNumber);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
+        }
+    }
+
+}
