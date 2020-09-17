427f02840de62aadd9afc00a4fe0d882ddce00a9
Kimkims1 <kimanthikevin76@gmail.com>
2020-09-17 03:34:11 +0300

    codes39

--- app/src/main/java/com/carldroid/itjobs/adapters/JobAdapter.java
+++ app/src/main/java/com/carldroid/itjobs/adapters/JobAdapter.java
@@ -1,6 +1,7 @@
 package com.carldroid.itjobs.adapters;
 
 import android.content.Context;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,10 +16,19 @@ import com.carldroid.itjobs.R;
 import com.carldroid.itjobs.models.JobModel;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class JobAdapter extends FirestoreRecyclerAdapter<JobModel, JobAdapter.jobViewHolder> {
 
     private Context context;
+    private CollectionReference reference;
+    private FirebaseFirestore firestore;
 
     /**
      * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
@@ -26,24 +36,68 @@ public class JobAdapter extends FirestoreRecyclerAdapter<JobModel, JobAdapter.jo
      *
      * @param options
      */
-    public JobAdapter(@NonNull FirestoreRecyclerOptions<JobModel> options,Context context) {
+    public JobAdapter(@NonNull FirestoreRecyclerOptions<JobModel> options, Context context) {
         super(options);
         this.context = context;
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull jobViewHolder holder, int position, @NonNull JobModel model) {
+    protected void onBindViewHolder(@NonNull final jobViewHolder holder, int position, @NonNull final JobModel model) {
         holder.jobBudget.setText("Budget: " + model.getJobBudget());
         holder.jobDescription.setText("Description: " + "\n" + model.getJobDescription());
         holder.jobDuration.setText("Time: " + model.getJobDuration());
         holder.payMethod.setText("Payment: " + model.getPayMethod());
         holder.jobTitle.setText("Title: " + model.getJobTitle());
         holder.idNumber.setText("Order id: " + model.getIdNumber());
-        
+
         holder.btnApply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(context, "Apply button clicked", Toast.LENGTH_SHORT).show();
+
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                CollectionReference collectionReference;
+
+                firestore = FirebaseFirestore.getInstance();
+
+                if (user != null) {
+                    // Name, email address, and profile photo Url
+                    String name = user.getDisplayName();
+                    String email = user.getEmail();
+                    Uri photoUrl = user.getPhotoUrl();
+
+                    // Check if user's email is verified
+                    boolean emailVerified = user.isEmailVerified();
+
+                    // The user's ID, unique to the Firebase project. Do NOT use this value to
+                    // authenticate with your backend server, if you have one. Use
+                    // FirebaseUser.getIdToken() instead.
+                    String uid = user.getUid();
+                }
+
+
+                collectionReference = firestore.collection("AppliedJobs");
+
+                String jobBudget = model.getJobBudget();
+                String jobDescription = model.getJobDescription();
+                String jobDuration = model.getJobDuration();
+                long idNumber = model.getIdNumber();
+                String payMethod = model.getPayMethod();
+                String jobTitle = model.getJobTitle();
+
+                JobModel jobModel = new JobModel(jobTitle, jobDescription, jobBudget, jobDuration, payMethod, idNumber);
+
+                collectionReference.add(jobModel)
+                        .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentReference> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(context, "Job applied! You will receive an email or phone call with further instructions", Toast.LENGTH_SHORT).show();
+
+                                } else {
+                                    Toast.makeText(context, "Application failed: " + task.getException(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
     }
